47b308697fe35152e8479f4c61e8d7e413e28908
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2020-07-27 23:24:00 -0300

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import Menu from "./components/Menu";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
